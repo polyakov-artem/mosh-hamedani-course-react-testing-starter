0e65ad7c589c76621e3204b5bd3719af526b832f
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-01 21:03:13 +0300

    build: add tests setup file, change vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: "jsdom",
+    setupFiles: ["tests/setup.ts"],
+    coverage: {
+      provider: "v8",
+      reportOnFailure: true,
+    },
   },
 });
