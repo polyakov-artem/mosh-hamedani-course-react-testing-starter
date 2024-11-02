b8698d3de67595b3378dbd064537ccef8e2831b6
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-02 20:25:05 +0300

    build: add window matchMedia property in setup.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -10,3 +10,17 @@ import { cleanup } from "@testing-library/react";
 afterEach(() => {
   cleanup();
 });
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
