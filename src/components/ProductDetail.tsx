7611c0ae8870e5a52e5ef6868694ef92ee857ec3
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-03 16:34:43 +0300

    fix: fix onmount callback to process data if response body is null

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import { Product } from "../entities";
 
 const ProductDetail = ({ productId }: { productId: number }) => {
-  const [product, setProduct] = useState<Product | undefined>(
-    undefined
-  );
+  const [product, setProduct] = useState<Product | undefined>(undefined);
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
@@ -16,11 +14,14 @@ const ProductDetail = ({ productId }: { productId: number }) => {
 
     setLoading(true);
     fetch("/products/" + productId)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 404) return null;
+        return res.json();
+      })
       .then((data) => setProduct(data))
       .catch((err) => setError((err as Error).message))
       .finally(() => setLoading(false));
-  }, []);
+  }, [productId]);
 
   if (isLoading) return <div>Loading...</div>;
 
