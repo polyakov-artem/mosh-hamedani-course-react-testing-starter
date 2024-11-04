752bd5f4c911d30f324bf639b393cbb2ac61d968
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-04 19:40:57 +0300

    fix : change mock constants

--- src/mocks/constants.tsx
+++ src/mocks/constants.tsx
@@ -1,11 +1,59 @@
 export const fetchedCategories = [
-  { id: 1, name: "Electronics" },
-  { id: 2, name: "Beauty" },
-  { id: 3, name: "Gardening" },
+  {
+    id: 1,
+    name: "Electronics",
+  },
+  {
+    id: 2,
+    name: "Appliances",
+  },
+  {
+    id: 3,
+    name: "Accessories",
+  },
 ];
 
 export const fetchedProducts = [
-  { id: 1, name: "Product 1", price: 1 },
-  { id: 2, name: "Product 2", price: 2 },
-  { id: 3, name: "Product 3", price: 3 },
+  {
+    id: 1,
+    name: "Refined Concrete Soap",
+    price: 799,
+    categoryId: 1,
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    price: 499,
+    categoryId: 1,
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: 149,
+    categoryId: 1,
+  },
+  {
+    id: 15,
+    name: "Robot Vacuum Cleaner",
+    price: 299,
+    categoryId: 2,
+  },
+  {
+    id: 16,
+    name: "Electric Toothbrush",
+    price: 39,
+    categoryId: 2,
+  },
+  {
+    id: 17,
+    name: "Wireless Router",
+    price: 89,
+    categoryId: 1,
+  },
+  {
+    id: 18,
+    name: "Bluetooth Headset",
+    price: 59,
+    categoryId: 1,
+  },
 ];
